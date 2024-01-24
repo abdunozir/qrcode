81dd0a727a171d819ec1a23021db3b4e9d054491
abdunozir <ibyvtnq@gmail.com>
2024-01-24 19:34:46 +0300

    new version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import QRCode from "qrcode.react";
+import domtoimage from "dom-to-image";
+import { useState } from "react";
 
 function App() {
+  let [data, setData] = useState("Default data");
+
+  const downloadQRCode = () => {
+    const node = document.getElementById("qrcode");
+
+    domtoimage
+      .toPng(node)
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.href = dataUrl;
+        link.download = "qrcode.png";
+        link.click();
+      })
+      .catch((error) => {
+        console.error("Error generating QR code:", error);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="qrcode_wrapper">
+        <input
+          type="text"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+          placeholder="type to generate qrcode"
+        />
+        <QRCode value={data} id="qrcode" />
+        <button onClick={downloadQRCode}>Download QR Code</button>
+      </div>
     </div>
   );
 }
